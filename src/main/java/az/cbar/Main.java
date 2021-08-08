bf5219e3a24de95ca64cfcc6ffd9b59263afb88f
sarvan Ibishov <sarvan.ibishov@a2z.az>
2021-08-08 17:46:12 +0400

    some cleanup

--- src/main/java/az/cbar/Main.java
+++ src/main/java/az/cbar/Main.java
@@ -1,24 +1,16 @@
 package az.cbar;
 
-import az.cbar.dao.CurrencyDao;
-import az.cbar.dao.impl.CurrencyDaoImpl;
-import az.cbar.entity.Currency;
 import az.cbar.model.ValType;
+import az.cbar.model.Valute;
 import az.cbar.service.parser.BaseParser;
 import az.cbar.service.parser.JaxBParser;
 import az.cbar.service.writer.BaseWriter;
 import az.cbar.service.writer.DatabaseWriter;
-
-
-import javax.transaction.Transactional;
-import java.math.BigDecimal;
 import java.util.List;
 
-import static az.cbar.dao.impl.EntityManagerImpl.getEntityManager;
-import static az.cbar.dao.impl.EntityManagerImpl.getTransaction;
 
 public class Main {
-    @Transactional
+
     public static void main(String[] args) {
         BaseParser<ValType> baseParser = new JaxBParser();
         List<ValType> valTypes = baseParser.parse();
@@ -26,11 +18,9 @@ public class Main {
                 .filter(valType1 -> valType1.getType().equals("Xarici valyutalar"))
                 .findFirst()
                 .orElseThrow(() -> new RuntimeException("Specified currency not found!"));
-        CurrencyDao currencyDao = new CurrencyDaoImpl();
 
-        BaseWriter baseWriter = new DatabaseWriter();
+        BaseWriter<Valute> baseWriter = new DatabaseWriter();
         baseWriter.write(valType.getValute());
-
 
     }
 
